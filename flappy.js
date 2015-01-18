df2ebf832516d2b34cc8514be530f35db60be8ce
richrobevans <re315@cam.ac.uk>
2015-01-18 16:31:37 +0000

    My game post lunch

--- flappy.js
+++ flappy.js
@@ -17,11 +17,12 @@ var player;
  * Loads all resources for the game and gives them names.
  */
 function preload() {
-game.load.image("playerImg", "assets/jamesBond.gif");
+game.load.image("playerImg", "assets/MarioPlayer2.jpg");
     game.load.image("Border", "assets/shroom.jpg");
-
     game.load.audio("score", "assets/point.ogg");
     game.load.audio("Mario1", "assets/sm64_mario_haha.wav");
+    game.load.image("pipe","assets/pipe.png");
+    game.load.audio("Game Over", "assets/sm64_game_over and music.wav");
 
 }
 
@@ -29,6 +30,7 @@ game.load.image("playerImg", "assets/jamesBond.gif");
  * Initialises the game. This function is only called once.
  */
 function create() {
+
     game.stage.setBackgroundColor("#3399FF");
     game.add.text(350, 180, "Not like Super Mario",{font:"40px Arial", fill: "#FFFFFF"});
     game.add.sprite(10, 10, "Border");
@@ -38,7 +40,7 @@ function create() {
 
     game.input.onDown.add(clickHandler);
 
-    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(spaceHandler);
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(player_jump);
 
     label_score = game.add.text(500, 20, "0",{font:"40px Arial", fill: "#FFFFFF"});
 
@@ -51,6 +53,21 @@ function create() {
     game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(moveUp);
 
     game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(moveDown);
+
+    pipes = game.add.group();
+    //generate_pipe();
+
+    pipe_interval = 2.25;
+    game.time.events.loop(pipe_interval * Phaser.Timer.SECOND, generate_pipe);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.physics.arcade.enable(player);
+    //player.body.velocity.x = 0;
+    player.body.velocity.y = -100;
+    player.body.gravity.y = 200;
+
+
     // set the background colour of the scene
 }
 
@@ -59,15 +76,17 @@ function create() {
  */
 function update() {
 
+    game.physics.arcade.overlap(player, pipes, game_over);
+
 
 }
 
 function clickHandler(event) {
-    game.add.sprite(event.x, event.y, "playerImg");
+    game.sound.play("Mario1");
 }
 
-function spaceHandler(event) {
-    game.sound.play("Mario1");
+function player_jump() {
+    player.body.velocity.y = -200;
 }
 
 function changeScore () {
@@ -89,4 +108,27 @@ function moveUp() {
 
 function moveDown() {
     player.y = player.y + 10
+}
+
+function generate_pipe(){
+    var gap = game.rnd.integerInRange(1, 5);
+    for (var count = 0; count < 8; count++) {
+        if (count != gap && count != gap + 1) {
+            add_pipe_block(800, count * 50);
+        }
+    }
+    changeScore();
+
+}
+
+function add_pipe_block(x, y) {
+    var pipe = pipes.create(x, y, "pipe");
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -200;
+}
+
+function game_over() {
+    game.add.text(350, 180, "At least your game is cooler than Alex's",{font:"50px Arial", fill: "#FFFFFF"});
+    game.sound.play("Game Over");
+    game.destroy();
 }
